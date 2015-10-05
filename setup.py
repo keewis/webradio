ccba1f3f490bb5723c60513b0419427900c64575
Keewis <keewis@posteo.de>
2015-10-05 15:28:57 +0200

    create a draft of setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+import webradio
+
+setup(
+    name='webradio',
+    description=webradio.__doc__.split("\n", 1)[0],
+    long_description=open("README.rst").read(),
+    version=webradio.__version__,
+    author="Justus",
+    author_email="justus.magin@posteo.de",
+    license='BSD',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        ],
+    packages=[
+        "webradio",
+        ],
+    )
