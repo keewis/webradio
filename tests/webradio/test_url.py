d16cb3e98992c207847ae60c1a53542c84837668
Keewis <keewis@posteo.de>
2015-10-06 01:45:28 +0200

    move the data defined in the individual test functions to global
    which allows reusing the data and not specifying it multiple times

--- tests/webradio/test_url.py
+++ tests/webradio/test_url.py
@@ -5,43 +5,48 @@ sys.modules['requests'] = mock.Mock()
 import webradio.url as url
 
 
-def test_urltype():
-    test_streams = [
-        "http://streams.br.de/bayern2sued_2.m3u",
-        "http://streams.br.de/b5aktuell_2.m3u",
-        "http://stream-sd.radioparadise.com:8056",
-        "http://icecast2.rte.ie/ieradio1",
-        ]
-    expected_types = [
-        "playlist",
-        "playlist",
-        "direct",
-        "direct",
-        ]
+urls = (
+    "http://streams.br.de/bayern2sued_2.m3u",
+    "http://stream-sd.radioparadise.com:8056",
+    "http://icecast2.rte.ie/ieradio1",
+    )
+url_types = (
+    "playlist",
+    "direct",
+    "direct",
+)
+content = (
+    "http://br-mp3-bayern2sued-m.akacast.akamaistream.net"
+    "/7/731/256282/v1/gnl.akacast.akamaistream.net/br_mp3_bayern2sued_m\n"
+    "http://br-mp3-bayern2sued-m.akacast.akamaistream.net"
+    "/7/731/256282/v1/gnl.akacast.akamaistream.net/br_mp3_bayern2sued_m\n"
+    "http://br-mp3-bayern2sued-s.akacast.akamaistream.net"
+    "/7/717/256282/v1/gnl.akacast.akamaistream.net/br_mp3_bayern2sued_s"
+    )
+extracted_url = (
+    "http://br-mp3-bayern2sued-m.akacast.akamaistream.net"
+    "/7/731/256282/v1/gnl.akacast.akamaistream.net/br_mp3_bayern2sued_m"
+    )
+prepared_urls = tuple(
+    url if index != 0 else extracted_url
+    for index, url in enumerate(urls)
+    )
 
-    types = list(map(url.urltype, test_streams))
+
+def test_urltype():
+    expected_types = url_types
+    types = tuple(map(url.urltype, urls))
     assert types == expected_types
 
 
 def test_extract_playlist():
-    text = (
-        "http://br-mp3-bayern2sued-m.akacast.akamaistream.net"
-        "/7/731/256282/v1/gnl.akacast.akamaistream.net/br_mp3_bayern2sued_m\n"
-        "http://br-mp3-bayern2sued-m.akacast.akamaistream.net"
-        "/7/731/256282/v1/gnl.akacast.akamaistream.net/br_mp3_bayern2sued_m\n"
-        "http://br-mp3-bayern2sued-s.akacast.akamaistream.net"
-        "/7/717/256282/v1/gnl.akacast.akamaistream.net/br_mp3_bayern2sued_s"
-        )
-
-    expected_url = (
-        "http://br-mp3-bayern2sued-m.akacast.akamaistream.net"
-        "/7/731/256282/v1/gnl.akacast.akamaistream.net/br_mp3_bayern2sued_m"
-        )
+    text = content
+    expected_url = extracted_url
     assert url.extract_playlist(text) == expected_url
 
 
 def test_acquire_playlist():
-    test_url = "http://streams.br.de/bayern2sued_2.m3u"
+    test_url = urls[0]
     expected_content = ""
 
     get = mock.Mock()
@@ -52,14 +57,7 @@ def test_acquire_playlist():
         assert url.acquire_playlist(test_url) == expected_content
 
     answer.reset_mock()
-    expected_content = (
-        "http://br-mp3-bayern2sued-m.akacast.akamaistream.net"
-        "/7/731/256282/v1/gnl.akacast.akamaistream.net/br_mp3_bayern2sued_m\n"
-        "http://br-mp3-bayern2sued-m.akacast.akamaistream.net"
-        "/7/731/256282/v1/gnl.akacast.akamaistream.net/br_mp3_bayern2sued_m\n"
-        "http://br-mp3-bayern2sued-s.akacast.akamaistream.net"
-        "/7/717/256282/v1/gnl.akacast.akamaistream.net/br_mp3_bayern2sued_s"
-        )
+    expected_content = content
     with mock.patch("requests.get", get):
         answer.ok = True
         answer.text = expected_content
@@ -70,38 +68,11 @@ def test_acquire_playlist():
 @mock.patch("webradio.url.acquire_playlist")
 @mock.patch("webradio.url.urltype")
 def test_prepare_stream_urls(urltype, acquire_playlist, extract_playlist):
-    urls = (
-        "http://streams.br.de/bayern2sued_2.m3u",
-        "http://stream-sd.radioparadise.com:8056",
-        "http://icecast2.rte.ie/ieradio1",
-        )
-    expected_streams = (
-        (
-            "http://br-mp3-bayern2sued-m.akacast.akamaistream.net"
-            "/7/731/256282/v1/gnl.akacast.akamaistream.net/"
-            "br_mp3_bayern2sued_m"
-            ),
-        "http://stream-sd.radioparadise.com:8056",
-        "http://icecast2.rte.ie/ieradio1",
-        )
+    expected_streams = prepared_urls
 
-    urltype.side_effect = (
-        "playlist",
-        "direct",
-        "direct",
-        )
-    acquire_playlist.side_effect = (
-        "http://br-mp3-bayern2sued-m.akacast.akamaistream.net"
-        "/7/731/256282/v1/gnl.akacast.akamaistream.net/br_mp3_bayern2sued_m\n"
-        "http://br-mp3-bayern2sued-m.akacast.akamaistream.net"
-        "/7/731/256282/v1/gnl.akacast.akamaistream.net/br_mp3_bayern2sued_m\n"
-        "http://br-mp3-bayern2sued-s.akacast.akamaistream.net"
-        "/7/717/256282/v1/gnl.akacast.akamaistream.net/br_mp3_bayern2sued_s",
-        )
-    extract_playlist.side_effect = (
-        "http://br-mp3-bayern2sued-m.akacast.akamaistream.net"
-        "/7/731/256282/v1/gnl.akacast.akamaistream.net/br_mp3_bayern2sued_m",
-        )
+    urltype.side_effect = url_types
+    acquire_playlist.side_effect = (content,)
+    extract_playlist.side_effect = (extracted_url,)
 
     prepared_streams = url.prepare_stream_urls(urls)
     assert prepared_streams == expected_streams
