b9a4f3ab6eb9776f5a1def069fc47edb8d9d2185
Keewis <keewis@posteo.de>
2015-10-05 22:33:51 +0200

    implement a playlist extraction function

--- tests/webradio/test_url.py
+++ tests/webradio/test_url.py
@@ -18,3 +18,20 @@ def test_urltype():
 
     types = list(map(url.urltype, test_streams))
     assert types == expected_types
+
+
+def test_extract_playlist():
+    text = (
+        "http://br-mp3-bayern2sued-m.akacast.akamaistream.net"
+        "/7/731/256282/v1/gnl.akacast.akamaistream.net/br_mp3_bayern2sued_m\n"
+        "http://br-mp3-bayern2sued-m.akacast.akamaistream.net"
+        "/7/731/256282/v1/gnl.akacast.akamaistream.net/br_mp3_bayern2sued_m\n"
+        "http://br-mp3-bayern2sued-s.akacast.akamaistream.net"
+        "/7/717/256282/v1/gnl.akacast.akamaistream.net/br_mp3_bayern2sued_s"
+        )
+
+    expected_url = (
+        "http://br-mp3-bayern2sued-m.akacast.akamaistream.net"
+        "/7/731/256282/v1/gnl.akacast.akamaistream.net/br_mp3_bayern2sued_m"
+        )
+    assert url.extract_playlist(text) == expected_url
--- webradio/url.py
+++ webradio/url.py
@@ -9,3 +9,11 @@ def urltype(url):
         return "direct"
     else:
         return "playlist"
+
+
+def extract_playlist(text):
+    urls = text.split("\n")
+    if len(urls) == 0:
+        raise RuntimeError("could not extract stream url")
+
+    return urls[0]
