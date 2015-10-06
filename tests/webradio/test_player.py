750c5f77d0aed0d9c5f90b40c00455e2b7b8f0b8
Keewis <keewis@posteo.de>
2015-10-06 23:53:33 +0200

    implement the destructor of the player class
    which ensures that the socket connection is closed

--- tests/webradio/test_player.py
+++ tests/webradio/test_player.py
@@ -16,3 +16,11 @@ class TestPlayer(object):
 
         assert isinstance(music_client, player.Player)
         client_mock.connect.called_once_with(host=socketpath, port=0)
+
+    @mock.patch("webradio.player.musicpd.MPDClient")
+    def test_destroy(self, mpdclient):
+        socketpath = ""
+        client_mock = mpdclient()
+        player.Player(socketpath)
+
+        assert client_mock.disconnect.call_count == 1
--- webradio/player.py
+++ webradio/player.py
@@ -5,3 +5,6 @@ class Player(object):
     def __init__(self, socketpath):
         self._client = musicpd.MPDClient()
         self._client.connect(host=socketpath, port=0)
+
+    def __del__(self):
+        self._client.disconnect()
