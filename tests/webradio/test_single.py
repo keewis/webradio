181e0c5a8d303a09734da16f1fd7a257ec8930a0
Keewis <keewis@posteo.de>
2016-09-21 22:15:59 +0200

    rewrite the client including its test suite

--- tests/webradio/test_single.py
+++ tests/webradio/test_single.py
@@ -81,6 +81,17 @@ def fill():
         yield fill
 
 
+@pytest.fixture(scope='function')
+def mpdclient():
+    m = mock.patch(
+        'webradio.single.musicpd.MPDClient',
+        mock.create_autospec(single.musicpd.MPDClient),
+        )
+
+    with m as mpdclient:
+        yield mpdclient
+
+
 def test_fill(tmpdir):
     path = pathlib.Path(str(tmpdir))
 
@@ -177,3 +188,278 @@ class TestServer(object):
         socket = s.socket
 
         assert socket.relative_to(basepath) == pathlib.Path("mpd/socket")
+
+
+class TestClient(object):
+    basepath = pathlib.Path("root")
+
+    def test_init(self, mpdclient):
+        client_mock = mpdclient.return_value
+
+        # default arguments
+        client = single.Client(self.basepath)
+
+        expected_calls = [
+            mock.call(host=str(self.basepath.absolute()), port=0),
+            ]
+        assert client_mock.connect.call_args_list == expected_calls
+        assert client.muted is False
+
+        # explicit muted state
+        muted = True
+        client = single.Client(self.basepath, muted=muted)
+
+        assert client.muted == muted
+
+    def test_destroy(self, mpdclient):
+        client_mock = mpdclient.return_value
+
+        client = single.Client(self.basepath)
+        del client
+
+        assert client_mock.disconnect.call_count == 1
+
+    def test_connect(self, mpdclient):
+        client_mock = mpdclient.return_value
+
+        client = single.Client(self.basepath)
+        client_mock.connect.reset_mock()
+
+        client._connect()
+
+        expected_call = mock.call(host=str(self.basepath.absolute()), port=0)
+        assert client_mock.connect.call_args_list == [expected_call]
+
+    def test_disconnect(self, mpdclient):
+        client_mock = mpdclient.return_value
+
+        client = single.Client(self.basepath)
+
+        # succeeding
+        client._disconnect()
+        assert client_mock.disconnect.call_count == 1
+
+        client_mock.disconnect.reset_mock()
+        # failing
+        client_mock.disconnect.side_effect = BrokenPipeError
+        client._disconnect()
+
+        assert client_mock.disconnect.call_count == 1
+
+    def test_ensure_connection(self, mpdclient):
+        client_mock = mpdclient.return_value
+
+        client = single.Client(self.basepath)
+
+        # succeeding
+        client_mock.ping.return_value = True
+        client.clear()
+        assert client_mock.clear.call_count == 1
+
+        client_mock.clear.reset_mock()
+        client_mock.connect.reset_mock()
+        # failing
+        client_mock.ping.side_effect = BrokenPipeError
+        client.clear()
+
+        # check that the calls were actually executed
+        assert client_mock.clear.call_count == 1
+        assert client_mock.connect.call_count == 1
+        assert client_mock.disconnect.call_count == 1
+
+    def test_volume(self, mpdclient):
+        client_mock = mpdclient.return_value
+
+        volume1 = 20
+        volume2 = 35
+        volume3 = 50
+
+        client_mock.status.return_value = {'volume': volume1}
+        client = single.Client(self.basepath, muted=False)
+
+        # initial volume
+        assert client.volume == volume1
+
+        # first change
+        client.volume = volume2
+        assert client.volume == volume2
+
+        # second change
+        client.volume = volume3
+        assert client.volume == volume3
+
+        # assert that the changes took effect on the server
+        expected_calls = [mock.call(volume2), mock.call(volume3)]
+        assert client_mock.setvol.call_args_list == expected_calls
+
+    def test_urls(self, mpdclient):
+        client_mock = mpdclient.return_value
+        client = single.Client(self.basepath)
+
+        urls1 = list(map(str, range(2, 7)))
+        urls2 = list(map(str, range(5, 40)))
+
+        # initial value
+        assert client.urls == []
+
+        # reassign once
+        client.urls = urls1
+        assert client.urls == urls1
+
+        # reassign again
+        client.urls = urls2
+        assert client.urls == urls2
+
+        # check the server state
+        assert client_mock.clear.call_count == 2
+        expected_calls = list(map(mock.call, urls1 + urls2))
+        assert client_mock.add.call_args_list == expected_calls
+
+    def test_add(self, mpdclient):
+        client_mock = mpdclient.return_value
+
+        # urls to add
+        urls = list(map(str, range(5)))
+
+        client = single.Client(self.basepath)
+        for url in urls:
+            client.add(url)
+
+        assert client_mock.add.call_args_list == list(map(mock.call, urls))
+        assert client._urls == urls
+
+    def test_clear(self, mpdclient):
+        client_mock = mpdclient.return_value
+
+        urls = list(map(str, range(20)))
+        client = single.Client(self.basepath)
+        client.urls = urls
+        client_mock.clear.reset_mock()
+
+        client.clear()
+        assert client_mock.clear.call_count == 1
+        assert client._urls == []
+
+    def test_play(self, mpdclient):
+        client_mock = mpdclient.return_value
+
+        length = 10
+        urls = list(map(str, range(length)))
+
+        client = single.Client(self.basepath)
+        client.urls = urls
+
+        # without index
+        client.play()
+
+        # succeeding
+        succeeding_index = abs(length - 5)
+        client.play(succeeding_index)
+
+        # failing
+        failing_index = length + 5
+        with pytest.raises(RuntimeError):
+            client.play(failing_index)
+
+        # check the server state
+        expected_calls = [mock.call(), mock.call(succeeding_index)]
+        assert client_mock.play.call_args_list == expected_calls
+
+    def test_muted(self, mpdclient):
+        client_mock = mpdclient.return_value
+
+        volume1 = 52
+        volume2 = 31
+        client_mock.status.return_value = {'volume': volume1}
+        client = single.Client(self.basepath, muted=False)
+
+        # unmuted to unmuted
+        client.muted = False
+        assert client.muted is False
+
+        # unmuted to muted
+        client.muted = True
+        assert client.muted is True
+
+        # muted to muted
+        client.muted = True
+        assert client.muted is True
+
+        # muted to unmuted
+        client.muted = False
+        assert client.muted is False
+
+        # check the server state
+        expected_calls = [mock.call(0), mock.call(volume1)]
+        assert client_mock.setvol.call_args_list == expected_calls
+
+        client_mock.setvol.reset_mock()
+
+        # check that the volume does behave correctly
+        # change when unmuted
+        client.volume = volume2
+
+        # change when muted
+        client.muted = True
+        client.volume = volume1
+
+        expected_calls = [mock.call(volume2), mock.call(0)]
+        assert client_mock.setvol.call_args_list == expected_calls
+
+        client_mock.setvol.reset_mock()
+        # only now when unmuting, the volume should change back again
+        client.muted = False
+        assert client_mock.setvol.call_args_list == [mock.call(volume1)]
+
+    def test_mute(self, mpdclient):
+        client = single.Client(self.basepath, muted=False)
+        client_mock = mpdclient.return_value
+
+        # mute when unmuted
+        client.mute()
+
+        assert client.muted is True
+
+        # mute when muted
+        client.mute()
+        assert client.muted is True
+
+        # the volume should only have changed once
+        assert client_mock.setvol.call_args_list == [mock.call(0)]
+
+    def test_unmute(self, mpdclient):
+        client_mock = mpdclient.return_value
+
+        volume = 41
+        client_mock.status.return_value = {'volume': volume}
+        client = single.Client(self.basepath, muted=True)
+
+        # unmute when muted
+        client.unmute()
+        assert client.muted is False
+
+        # unmute when unmuted
+        client.unmute()
+        assert client.muted is False
+
+        # the volume should only have changed once
+        assert client_mock.setvol.call_args_list == [mock.call(volume)]
+
+    def test_toggle_mute(self, mpdclient):
+        client_mock = mpdclient.return_value
+
+        volume = 54
+        client_mock.status.return_value = {'volume': volume}
+        client = single.Client(self.basepath, muted=False)
+
+        # toggle to muted
+        client.toggle_mute()
+        assert client.muted is True
+
+        # toggle to unmuted
+        client.toggle_mute()
+        assert client.muted is False
+
+        # the volume should have changed twice
+        expected_calls = [mock.call(0), mock.call(volume)]
+        assert client_mock.setvol.call_args_list == expected_calls
--- webradio/single.py
+++ webradio/single.py
@@ -1,6 +1,10 @@
+import musicpd
+from functools import wraps
 import pathlib
 import subprocess
 import shutil
+
+from . import base
 
 
 config_template = """
@@ -74,3 +78,119 @@ class Server(object):
             self.basepath.rmdir()
         except (FileNotFoundError, OSError):
             pass
+
+
+class Client(base.base_client):
+    def __init__(self, server, *, muted=False):
+        try:
+            self.basepath = server.socket
+        except AttributeError:
+            self.basepath = pathlib.Path(server).absolute()
+
+        self._client = musicpd.MPDClient()
+        self._connect()
+
+        self._muted = muted
+        self._volume = self._get_volume()
+
+        self._urls = []
+
+    def __del__(self):
+        self._disconnect()
+
+    def ensure_connection(func):
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            try:
+                self._client.ping()
+            except BrokenPipeError:
+                print("reconnecting")
+                self._reconnect()
+
+            return func(self, *args, **kwargs)
+
+        return wrapper
+
+    def _reconnect(self):
+        self._disconnect()
+        self._connect()
+
+    def _disconnect(self):
+        try:
+            self._client.disconnect()
+            print("disconnecting")
+        except BrokenPipeError:
+            pass
+
+    def _connect(self):
+        self._client.connect(host=str(self.basepath), port=0)
+
+    @ensure_connection
+    def _get_volume(self):
+        return int(self._client.status().get('volume'))
+
+    @ensure_connection
+    def _set_volume(self, new_volume):
+        self._client.setvol(new_volume)
+
+    @property
+    def volume(self):
+        return self._volume
+
+    @volume.setter
+    def volume(self, new_volume):
+        if not self.muted:
+            self._set_volume(int(new_volume))
+
+        self._volume = int(new_volume)
+
+    @property
+    def urls(self):
+        return self._urls
+
+    @urls.setter
+    def urls(self, urls):
+        self.clear()
+        for url in urls:
+            self.add(url)
+
+    @ensure_connection
+    def add(self, url):
+        self._client.add(url)
+        self._urls.append(url)
+
+    @ensure_connection
+    def clear(self):
+        self._client.clear()
+        self._urls = []
+
+    @ensure_connection
+    def play(self, index=None):
+        if index is None:
+            self._client.play()
+        else:
+            if index >= len(self._urls):
+                raise RuntimeError("invalid song index")
+            self._client.play(index)
+
+    @property
+    def muted(self):
+        return self._muted
+
+    @muted.setter
+    def muted(self, new_state):
+        if self._muted == new_state:
+            # if the required new state is the current one
+            return
+
+        self._muted = bool(new_state)
+        self._set_volume(0 if self._muted else self._volume)
+
+    def mute(self):
+        self.muted = True
+
+    def unmute(self):
+        self.muted = False
+
+    def toggle_mute(self):
+        self.muted = not self.muted
