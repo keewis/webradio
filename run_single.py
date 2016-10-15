14e99c5e84ef301488185858c3e330a3fc3d4303
Keewis <keewis@posteo.de>
2016-10-15 09:56:52 +0200

    add a single server running script using the second set of urls
    a highlight for this one is that it uses a temporary directory in /tmp
    as base path -- so no clobbering of the working directory necessary.

---
+++ run_single.py
@@ -0,0 +1,41 @@
+from contextlib import contextmanager
+import pathlib
+import tempfile
+
+from webradio import single, url
+
+
+@contextmanager
+def basepath(suffix):
+    try:
+        root = pathlib.Path(tempfile.mkdtemp(dir='/tmp'))
+        yield root / suffix
+    finally:
+        root.rmdir()
+
+suffix = "webradio"
+filepath = "urls2"
+with open(filepath) as filelike:
+    raw_urls = [line.strip() for line in filelike]
+    urls = [url.extract_playlist(_) for _ in raw_urls]
+
+
+def print_choices(urls):
+    for index, _url in enumerate(urls):
+        print("({}): {}".format(index, _url))
+
+with basepath(suffix) as path, single.map(path, urls) as client:
+    client.volume = 50
+    print(client.server.socket)
+    print_choices(urls)
+    while True:
+        try:
+            index = input("entry? ")
+            client.play(int(index))
+        except ValueError:
+            continue
+        except RuntimeError:
+            print_choices(urls)
+        except EOFError:
+            print("")
+            break
