0a87aba2c6181a0f2fb07b08e0a6cd772384e556
Keewis <keewis@posteo.de>
2016-10-16 18:56:32 +0200

    rename the test fixtures to describe whether they mean pool or single

--- tests/webradio/test_pool.py
+++ tests/webradio/test_pool.py
@@ -6,7 +6,7 @@ import webradio.pool as pool
 
 
 @pytest.fixture(scope='function')
-def server():
+def single_server():
     m = mock.patch(
         'webradio.pool.single.Server',
         mock.create_autospec(pool.single.Server),
@@ -17,10 +17,32 @@ def server():
 
 
 @pytest.fixture(scope='function')
-def client():
+def single_client():
     m = mock.patch(
         'webradio.pool.single.Client',
         mock.create_autospec(pool.single.Client),
+        )
+
+    with m as client:
+        yield client
+
+
+@pytest.fixture(scope='function')
+def pool_server():
+    m = mock.patch(
+        'webradio.pool.Server',
+        mock.create_autospec(pool.Server),
+        )
+
+    with m as server:
+        yield server
+
+
+@pytest.fixture(scope='function')
+def pool_client():
+    m = mock.patch(
+        'webradio.pool.Client',
+        mock.create_autospec(pool.Client),
         )
 
     with m as client:
@@ -39,7 +61,7 @@ def path():
 
 
 class TestServer(object):
-    def test_init(self, server, path):
+    def test_init(self, single_server, path):
         basepath = "/pool"
         n_instances = 10
 
@@ -55,9 +77,9 @@ class TestServer(object):
         pool.Server(basepath=basepath, num=n_instances)
 
         # check the number of server calls
-        assert server.call_count == n_instances
+        assert single_server.call_count == n_instances
 
-    def test_shutdown(self, server, path):
+    def test_shutdown(self, single_server, path):
         basepath = "/pool"
         n_instances = 10
 
@@ -74,7 +96,7 @@ class TestServer(object):
         # shut it down a second time (which should be a no-op)
         s.shutdown()
 
-        assert server.return_value.shutdown.call_count == n_instances
+        assert single_server.return_value.shutdown.call_count == n_instances
         assert basepath.rmdir.call_count == 1
 
         # raising an OSError
@@ -86,13 +108,13 @@ class TestServer(object):
         # shut it down
         s.shutdown()
 
-    def test_sockets(self, server, path):
+    def test_sockets(self, single_server, path):
         basepath = "/pool"
         n_instances = 10
         expected_socket_paths = list(map(str, range(n_instances)))
 
         basepath = path.return_value
-        server_prototype = server.return_value
+        server_prototype = single_server.return_value
         type(server_prototype).socket = mock.PropertyMock(
             side_effect=expected_socket_paths,
             )
@@ -106,35 +128,35 @@ class TestServer(object):
 
 
 class TestClient(object):
-    def test_init(self, client, server):
+    def test_init(self, single_client, pool_server):
         n_instances = 10
 
-        server_instance = server.return_value
+        server_instance = pool_server.return_value
         type(server_instance).sockets = mock.PropertyMock(
             return_value=range(n_instances),
             )
         muted = mock.PropertyMock()
-        type(client.return_value).muted = muted
+        type(single_client.return_value).muted = muted
 
         pool.Client(server_instance)
 
         # single client construction
         calls = list(
             call[1]['server']
-            for call in client.call_args_list
+            for call in single_client.call_args_list
             )
         assert calls == list(range(n_instances))
 
         # single client muted state
         assert muted.call_args_list == [mock.call(True)] * n_instances
 
-    def test_volume(self, client, server):
+    def test_volume(self, single_client, pool_server):
         n_instances = 10
         volume1 = 30
         volume2 = 45
 
-        server_instance = server.return_value
-        client_instance = client.return_value
+        server_instance = pool_server.return_value
+        client_instance = single_client.return_value
         type(server_instance).sockets = mock.PropertyMock(
             return_value=range(n_instances),
             )
