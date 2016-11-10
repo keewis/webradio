f3b070ed5f327deacf175761d20201ca11e2563c
Keewis <keewis@posteo.de>
2016-11-10 19:28:54 +0100

    remove the global content and extract_url variables
    which were used for m3u extraction tests. However, this function has its
    own test data now, so these global variables (which are often a bad
    idea) are not needed anymore.

--- tests/webradio/test_url.py
+++ tests/webradio/test_url.py
@@ -91,18 +91,6 @@ url_types = (
     "direct",
     "direct",
 )
-content = (
-    "http://br-mp3-bayern2sued-m.akacast.akamaistream.net"
-    "/7/731/256282/v1/gnl.akacast.akamaistream.net/br_mp3_bayern2sued_m\n"
-    "http://br-mp3-bayern2sued-m.akacast.akamaistream.net"
-    "/7/731/256282/v1/gnl.akacast.akamaistream.net/br_mp3_bayern2sued_m\n"
-    "http://br-mp3-bayern2sued-s.akacast.akamaistream.net"
-    "/7/717/256282/v1/gnl.akacast.akamaistream.net/br_mp3_bayern2sued_s"
-    )
-extracted_url = (
-    "http://br-mp3-bayern2sued-m.akacast.akamaistream.net"
-    "/7/731/256282/v1/gnl.akacast.akamaistream.net/br_mp3_bayern2sued_m"
-    )
 
 
 def test_urltype():
