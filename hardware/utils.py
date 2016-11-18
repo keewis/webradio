545bcd3f64abe72524d37d66491404df01ed2ad8
Keewis <keewis@posteo.de>
2016-11-18 14:36:21 +0100

    reimplement the splitting function
    which now can work on any sequence, not just lists and tuples.

--- hardware/utils.py
+++ hardware/utils.py
@@ -1,22 +1,32 @@
-def split_into_equal_parts(input_list, number_of_parts):
+import itertools
+
+
+def split_into_equal_parts(input_sequence, number_of_parts):
     """ Splits a list into a list of sublists
 
     Arguments
     ----------
-    a_list : list object
-        List wich will be split into Sublists
+    input_sequence : sequence
+        Sequence which will be split into approximately evenly sized iterables.
+        In contrast to other approaches, this ensures that the median iterable
+        is the largest (if necessary). The reason why this has to be a
+        sequence is that for computing the number of elements per iterable
+        we need to get the total number of elements via `len()` and to access
+        individual elements via subscripting.
     number_of_parts : int
-        Number of sublists which should be created
+        Number of iterables which are to be created
 
-    Returns
-    -------
-    seperated_lists : list of lists
-        List of Sublists of the given list which are as equal sized as possible
+    Yields
+    ------
+    seperated_iterable : iterable of iterables
+        iterable of iterables which are as evenly sized as possible
     """
-    start = 0
-    separated_lists = []
-    for i in range(number_of_parts):
-        end = round((len(input_list) / number_of_parts) * (i + 1))
-        separated_lists.append(input_list[start:end])
-        start = end
-    return separated_lists
+    total_length = len(input_sequence)
+    ratio = total_length / number_of_parts
+    indices = (
+        (round(ratio * i), round(ratio * (i + 1)))
+        for i in range(number_of_parts)
+        )
+
+    for start, end in indices:
+        yield itertools.islice(input_sequence, start, end)
