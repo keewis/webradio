d284a07d8920d0bef0ff4f9dec5efe81c4dcb8a7
Keewis <keewis@posteo.de>
2016-12-26 22:40:12 +0100

    outsource most of the async code in a player independant fashion
    however, this is experimental, and I'm not entirely convinced this is a
    nice module -- rather, this seems somewhat hacky. Definitly something to
    refactor in a week or two.

--- frontend/asynchronous.py
+++ frontend/asynchronous.py
@@ -1,5 +1,8 @@
 import asyncio
 from contextlib import contextmanager
+import sys
+
+from . import utils
 
 
 @contextmanager
@@ -11,3 +14,57 @@ def run_loop_forever():
         loop.run_forever()
     finally:
         loop.close()
+
+
+@asyncio.coroutine
+def print_prompt():
+    print(utils.prompt, end='', flush=True)
+
+
+@asyncio.coroutine
+def print_choices(urls):
+    print(utils.format_urls(urls))
+
+
+@asyncio.coroutine
+def switch_channel(client, index):
+    client.play(index)
+
+
+@asyncio.coroutine
+def change_volume(client, new_volume):
+    client.volume = new_volume
+
+
+@asyncio.coroutine
+def toggle_mute(client):
+    client.toggle_mute()
+
+
+@asyncio.coroutine
+def process_input(client):
+    try:
+        data = sys.stdin.readline()
+        if len(data.strip()) == 0:
+            yield from print_prompt()
+            return
+            # raise EOFError("end of program")
+
+        if data.strip() == "mute":
+            yield from toggle_mute(client)
+        elif "vol" in data:
+            _, new_volume = data.strip().split()
+            yield from change_volume(client, int(new_volume))
+        elif "help" in data:
+            raise ValueError()
+        else:
+            yield from switch_channel(client, int(data))
+
+        yield from print_prompt()
+    except (ValueError, RuntimeError):
+        yield from print_choices(client.urls)
+        yield from print_prompt()
+    except (EOFError, KeyboardInterrupt):
+        print("")
+        loop = asyncio.get_event_loop()
+        loop.stop()
--- tests/frontend/test_asynchronous.py
+++ tests/frontend/test_asynchronous.py
@@ -1,6 +1,80 @@
-import frontend.asynchronous as asynchronous
 import asyncio
+from contextlib import contextmanager, redirect_stdout
+import io
+import sys
+from unittest import mock
+
 import pytest
+
+import frontend.asynchronous as asynchronous
+
+
+@pytest.fixture(scope='function')
+def stdin():
+    m = mock.patch(
+        'sys.stdin',
+        new_callable=io.StringIO,
+        )
+
+    with m as stdin:
+        yield stdin
+
+
+@contextmanager
+def print_to_stdin():
+    stdout = io.StringIO()
+    with redirect_stdout(stdout):
+        yield
+    sys.stdin.write(stdout.getvalue())
+    sys.stdin.seek(0)
+
+
+@contextmanager
+def reset_file(filelike):
+    try:
+        yield
+    finally:
+        filelike.truncate(0)
+        filelike.seek(0)
+
+
+@pytest.fixture(scope='function')
+def client():
+    m = mock.Mock(
+        name='fake_client',
+        spec=[
+            'volume',
+            'toggle_mute',
+            'play',
+            'urls',
+            ],
+        )
+    yield m
+
+
+@contextmanager
+def mock_loop():
+    old_loop = asyncio.get_event_loop()
+    try:
+        new_loop = mock.create_autospec(old_loop)
+        asyncio.set_event_loop(new_loop)
+        yield new_loop
+    finally:
+        asyncio.set_event_loop(old_loop)
+
+
+@contextmanager
+def readline_sideeffect(exception):
+    m = mock.patch(
+        'sys.stdin.readline',
+        new_callable=mock.Mock,
+        )
+    with m as readline:
+        try:
+            readline.side_effect = exception
+            yield readline
+        finally:
+            readline.side_effect = None
 
 
 def test_run_loop_forever():
@@ -20,3 +94,122 @@ def test_run_loop_forever():
             long_running = task(10)
             ensure_future(long_running)
             loop.run_until_complete(task(0.01))
+
+
+@pytest.mark.asyncio
+def test_print_choices(capsys):
+    urls = list(map(lambda x: 'x{}'.format(x), range(9)))
+    yield from asynchronous.print_choices(urls)
+
+    formatted_urls = asynchronous.utils.format_urls(urls)
+    expected_output = formatted_urls + "\n"
+
+    output, _ = capsys.readouterr()
+    assert output == expected_output
+
+
+@pytest.mark.asyncio
+def test_process_input(client, stdin, capsys):
+    urls = list(map(str, range(9)))
+    client.urls = urls
+
+    # mute
+    with reset_file(stdin):
+        with print_to_stdin():
+            print("mute", flush=True)
+
+        yield from asynchronous.process_input(client)
+        output, _ = capsys.readouterr()
+        assert client.toggle_mute.call_count == 1
+        assert output == asynchronous.utils.prompt
+
+    # vol
+    with reset_file(stdin):
+        new_volume = 10
+        with print_to_stdin():
+            print("vol {}".format(new_volume), flush=True)
+
+        yield from asynchronous.process_input(client)
+        output, _ = capsys.readouterr()
+        assert client.volume == new_volume
+        assert output == asynchronous.utils.prompt
+
+    # station
+    with reset_file(stdin):
+        new_channel = 1
+        with print_to_stdin():
+            print("{}".format(new_channel), flush=True)
+
+        yield from asynchronous.process_input(client)
+        output, _ = capsys.readouterr()
+        assert client.play.call_args_list == [mock.call(new_channel)]
+        assert output == asynchronous.utils.prompt
+        client.play.reset_mock()
+
+    # invalid station
+    with reset_file(stdin):
+        new_channel = 15
+        client.play.side_effect = RuntimeError
+
+        with print_to_stdin():
+            print("{}".format(new_channel), flush=True)
+
+        yield from asynchronous.process_input(client)
+
+        output, _ = capsys.readouterr()
+        expected_output = "\n".join([
+            asynchronous.utils.format_urls(urls),
+            asynchronous.utils.prompt,
+            ])
+
+        assert client.play.call_args_list == [mock.call(new_channel)]
+        assert output == expected_output
+
+        client.play.reset_mock()
+
+    # invalid value
+    with reset_file(stdin):
+        with print_to_stdin():
+            print("vol19d", flush=True)
+
+        yield from asynchronous.process_input(client)
+        output, _ = capsys.readouterr()
+        expected_output = "\n".join([
+            asynchronous.utils.format_urls(urls),
+            asynchronous.utils.prompt,
+            ])
+        assert output == expected_output
+
+    # help
+    with reset_file(stdin):
+        with print_to_stdin():
+            print("help", flush=True)
+
+        yield from asynchronous.process_input(client)
+        output, _ = capsys.readouterr()
+        expected_output = "\n".join([
+            asynchronous.utils.format_urls(urls),
+            asynchronous.utils.prompt,
+            ])
+        assert output == expected_output
+
+    # empty
+    with reset_file(stdin):
+        yield from asynchronous.process_input(client)
+        output, _ = capsys.readouterr()
+        expected_output = asynchronous.utils.prompt
+        assert output == expected_output
+
+    # EOFError
+    with readline_sideeffect(EOFError), mock_loop() as loop:
+        yield from asynchronous.process_input(client)
+        output, _ = capsys.readouterr()
+        assert loop.stop.call_count == 1
+        assert "\n" == output
+
+    # KeyboardInterrupt
+    with readline_sideeffect(KeyboardInterrupt), mock_loop() as loop:
+        yield from asynchronous.process_input(client)
+        output, _ = capsys.readouterr()
+        assert loop.stop.call_count == 1
+        assert "\n" == output
