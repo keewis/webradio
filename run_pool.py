1bdb8b20767d6454f2e1b535626f235db3eb7f63
Keewis <keewis@posteo.de>
2017-01-15 17:58:25 +0100

    explicitly expire the basepath after the client

--- run_pool.py
+++ run_pool.py
@@ -9,11 +9,12 @@ with open(filepath) as filelike:
     raw_urls = [line.strip() for line in filelike]
     urls = [url.extract_playlist(_) for _ in raw_urls]
 
-with basepath(suffix) as path, pool.map(path, urls) as client:
-    synchronous.print_choices(urls)
-    synchronous.print_prompt()
-    while True:
-        try:
-            synchronous.process_input(client)
-        except StopIteration:
-            break
+with basepath(suffix) as path:
+    with pool.map(path, urls) as client:
+        synchronous.print_choices(urls)
+        synchronous.print_prompt()
+        while True:
+            try:
+                synchronous.process_input(client)
+            except StopIteration:
+                break
