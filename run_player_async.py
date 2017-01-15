14c80207d616137574000688f0618ac6c1b717ba
Keewis <keewis@posteo.de>
2017-01-15 19:45:59 +0100

    create a async player script
    which, thanks to the recent efforts, is only marginally different from
    the async pool script

---
+++ run_player_async.py
@@ -0,0 +1,36 @@
+from webradio import player, url
+from frontend.utils import basepath
+from frontend import asynchronous
+import asyncio
+import sys
+
+
+def read_urls(filelike):
+    raw_urls = [line.strip() for line in filelike]
+    urls = [url.extract_playlist(_) for _ in raw_urls]
+    return urls
+
+
+def reader(pool):
+    task = asyncio.async(asynchronous.process_input(pool))
+    asyncio.wait(task)
+
+
+if __name__ == "__main__":
+    suffix = "webradio_pool"
+    filepath = "urls"
+    with open(filepath) as filelike:
+        urls = read_urls(filelike)
+
+    asynchronous.actions['prebuffering'] = lambda *, client: setattr(
+        client,
+        "prebuffering",
+        not client.prebuffering,
+        )
+
+    with basepath(suffix) as p:
+        with player.map(basepath=p, urls=urls, prebuffering=False) as c, \
+                asynchronous.run_loop_forever() as loop:
+            asyncio.async(asynchronous.print_choices(c.urls))
+            asyncio.async(asynchronous.print_prompt())
+            loop.add_reader(sys.stdin, reader, c)
