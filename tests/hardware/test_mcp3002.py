a11aeaa9b4fe637243ce41307058b9f46783846b
Keewis <keewis@posteo.de>
2017-01-20 22:34:35 +0100

    only mock the SPI transfer method instead of the whole object

--- tests/hardware/test_mcp3002.py
+++ tests/hardware/test_mcp3002.py
@@ -6,23 +6,23 @@ import hardware.mcp3002 as mcp3002
 
 
 @pytest.fixture(scope='function')
-def mcp_spi():
+def spi_transfer():
     m = mock.patch(
-        'hardware.mcp3002.MCP3002._spi',
-        mock.create_autospec(_gpiozero.MCP3002._spi),
+        'hardware.mcp3002.MCP3002._spi.transfer',
+        mock.create_autospec(_gpiozero.MCP3002._spi.transfer),
         )
 
-    with m as _spi:
-        yield _spi
+    with m as transfer:
+        yield transfer
 
 
-def test_mcp3002_read(mcp_spi):
+def test_mcp3002_read(spi_transfer):
     channel = 0
     adc = mcp3002.MCP3002(channel=channel)
 
     expected_message = [192, 0]
     raw_result = [7, 250]
-    mcp_spi.transfer.return_value = raw_result
+    spi_transfer.return_value = raw_result
     expected_result = sum(
         value * 2 ** (index * 8)
         for index, value in enumerate(reversed(raw_result))
@@ -30,4 +30,4 @@ def test_mcp3002_read(mcp_spi):
 
     result = adc._read()
     assert result == expected_result
-    assert mcp_spi.transfer.call_args_list[0] == mock.call(expected_message)
+    assert spi_transfer.call_args_list[0] == mock.call(expected_message)
